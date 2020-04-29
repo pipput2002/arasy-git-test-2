e9e3c97a3611e505a9d5a998b92b96319ac6092a
pipput2002 <arasyyosida@gmail.com>
2020-04-29 10:03:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nobis. Praesentium illum laudantium vero, quos corrupti earum mollitia repellendus nam odio quibusdam ad excepturi natus. Dolore, fugit sit. Quod, esse?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
+
 </html>
